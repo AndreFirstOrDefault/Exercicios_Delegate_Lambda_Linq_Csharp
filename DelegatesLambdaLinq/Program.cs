7c19a03b8cca74f4a58b36ac48425812f1218d86
andrejava2022 <andre.java.ads.2022@gmail.com>
2024-01-04 13:37:44 -0300

    Exercicio 02 resolvido

--- DelegatesLambdaLinq/Program.cs
+++ DelegatesLambdaLinq/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+2- Implemente um programa que use delegates para somar dois números inteiros. O programa deve seguir 
+as seguintes especificações:
+
+ a-Defina um delegate chamado OperacaoMatematica que recebe dois inteiros como parâmetros e retorna 
+    um inteiro.
+ b-Defina um método que implementam a operação de soma: que recebe os dois inteiros como parâmetros 
+ c-Crie um objeto delegate do tipo OperacaoMatematica e atribua a ele ao método definido.
+ d-Chame o delegate passando os parâmetros necessários e exiba o resultado da operação na tela.
+  
+ */
+
+Console.Title = "02 - Delegate";
+
+// b
+static int Somar(int x, int y)
+{
+    return x + y;
+}
+
+// c
+OperacaoMatematica operacaoSoma = Somar;
+
+// d
+Console.WriteLine($"Resultado da operação com delegate: {operacaoSoma(50,30)}");
+Console.WriteLine($"Resultado da operação com delegate: {operacaoSoma.Invoke(30,70)}");
+
+Console.ReadKey();
+
+// a
+public delegate int OperacaoMatematica(int numero1, int numero2);
