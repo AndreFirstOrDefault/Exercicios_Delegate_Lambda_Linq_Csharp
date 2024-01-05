bfcdbb02f334f8ad6ce99b8a0b1a83ee8c2223ad
andrejava2022 <andre.java.ads.2022@gmail.com>
2024-01-05 12:02:29 -0300

    Exercicio 02 sobre linq

--- Exercicio02_Linq/Program.cs
+++ Exercicio02_Linq/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Exercício 2: Utilizando o método OrderBy
+
+Dado um array de inteiros, escreva um programa em C# que use a biblioteca LINQ para ordenar os valores 
+em ordem crescente.
+*/
+
+// Gerando um array de inteiros
+int[] arrayInteiros = new int[] { 5,2,16,200,7,125,0,82,3,98,1,22};
+
+Console.WriteLine("\nArray antes de ordenar...");
+
+foreach (var item in arrayInteiros)
+{
+    Console.Write(item + " ");
+}
+
+Console.WriteLine("\n\nArray após ordenar...");
+OrdenaArrayInteiros(arrayInteiros);
+
+Console.ReadKey();
+
+static void OrdenaArrayInteiros(int[] arrayInteiros)
+{
+    var arrayOrdenado = arrayInteiros.OrderBy(x => x).ToArray();
+    foreach (var item in arrayOrdenado)
+    {
+        Console.Write(item + " ");
+    }
+}
+
+
