0bc29c5da8d2fe3e4dacc2b475c41e4567bcb5a1
andrejava2022 <andre.java.ads.2022@gmail.com>
2024-01-05 12:38:32 -0300

    Finalizado a lista de Exercícios

---
+++ Exercicio03Linq/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+Exercício 3: Utilizando o método GroupBy
+
+Dado um array de strings, escreva um programa em C# que use a biblioteca LINQ para agrupar as strings por 
+tamanho 
+*/
+
+// Criando um array de strings
+string[] arrayString = new string[] {"marrom", "rato", "pato", "Brasil", "computador","mesa", "ferramenta", "hospital","oficina" };
+
+var grupos = arrayString.GroupBy(s => s.Length);
+                    
+foreach(var grupo in grupos)
+{
+    Console.WriteLine($"Strings de tamanho {grupo.Key}");
+    foreach (var item in grupo)
+    {
+        Console.WriteLine($" {item}");
+    }
+    Console.WriteLine();
+}
+
+Console.ReadKey();
---
+++ Exercicio04Linq/Program.cs
@@ -0,0 +1,14 @@
+﻿/*
+Exercício 4: Utilizando o método FirstOrDefault
+
+Dado um array de inteiros, escreva um programa em C# que use a biblioteca LINQ para obter o primeiro 
+número par do array. 
+*/
+
+int[] arrayInteiros = new int[] {15,89,33,5,77,200,204,9,10 };
+
+var primeiroPar = arrayInteiros.FirstOrDefault(x => x%2 == 0);
+
+Console.WriteLine($"\nPrimeiro número par encontrado no array: {primeiroPar}");
+
+Console.ReadKey();
